7e1ae9f34c5122672657990a3919f6f9d72fac5a
Starfyre <Starfyre@10.0.0.219>
2024-10-17 14:27:35 -0700

    initial commit

---
+++ Space3D/src/com/starfyre1/space3D/Startup.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.starfyre1.space3D;
+
+/**
+ * 
+ */
+public class Startup {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ 
